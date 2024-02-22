98056989481e48733182ee538ff562fbd3ec60f1
Wade Coats Jr. <149808971+WCC901@users.noreply.github.com>
2024-02-22 01:57:26 -0500

    Quick fix to previous commit

--- server.js
+++ server.js
@@ -2,11 +2,22 @@ require(`dotenv`).config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const port = process.env.PORT;
+const mongoURI = process.env.MONGO_URI;
 
 const app = express();
 
-const port = process.env.PORT;
-
-app.listen(port, () => {
-    console.log(`App listening on port: ${port}`);
+app.get("/", (req, res) => {
+    console.log(req);
+    return res.status(202).send("GeoShop Homepage!")
 });
+
+mongoose.connect(mongoURI)
+    .then(() => {
+        console.log("Connected to MongoDB!");
+        app.listen(port, () => {
+            console.log(`App listening on port: ${port}`);
+        });
+    }).catch((e) => {
+        console.log(e);
+    });
